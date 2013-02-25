00e26019acd50d6df67970e23d9face0eea6f979
Geoff Selby <geoff.selby.android@gmail.com>
2013-02-25 14:10:26 -0600

    third commit

--- application/config/config.php
+++ application/config/config.php
@@ -14,8 +14,8 @@
 | path to your installation.
 |
 */
-// $config['base_url']	= 'http://whitelion.boldcreativesolutions.com/';
-$config['base_url']	= 'http://dev.whitelion.com/';
+$config['base_url']	= 'http://whitelion.boldcreativesolutions.com/';
+
 
 /*
 |--------------------------------------------------------------------------
--- application/config/database.php
+++ application/config/database.php
@@ -48,21 +48,6 @@
 $active_group = 'default';
 $active_record = TRUE;
 
-// $db['default']['hostname'] = 'localhost';
-// $db['default']['username'] = 'boldcre1';
-// $db['default']['password'] = 'The12sf1!';
-// $db['default']['database'] = 'boldcre1_whitelion';
-// $db['default']['dbdriver'] = 'mysql';
-// $db['default']['dbprefix'] = '';
-// $db['default']['pconnect'] = TRUE;
-// $db['default']['db_debug'] = TRUE;
-// $db['default']['cache_on'] = FALSE;
-// $db['default']['cachedir'] = '';
-// $db['default']['char_set'] = 'utf8';
-// $db['default']['dbcollat'] = 'utf8_general_ci';
-// $db['default']['swap_pre'] = '';
-// $db['default']['autoinit'] = TRUE;
-// $db['default']['stricton'] = FALSE;
 
 $db['default']['hostname'] = 'localhost';
 $db['default']['username'] = 'root';
